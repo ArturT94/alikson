dac0242ff7cdf8f243c3c30c2618bede4cafee7e
Artur <gunsmoke94@mail.ru>
2022-09-29 08:22:35 +0300

    удаление шаблона aspro_maxx

--- local/templates/aspro_maxx/components/aspro/wrapper.block.max/front_vk/lang/en/template.php
+++
@@ -1,7 +0,0 @@
-<?
-$MESS["PREV_LINK"] = "Back";
-$MESS["MAX_LINK"] = "Forward";
-$MESS["CURRENT_PAGE"] = "Page:";
-$MESS["MORE_LINK"] = "More on Yandex.Market";
-$MESS ['REVIEWS_LOAD'] = "Loading...";
-?>
